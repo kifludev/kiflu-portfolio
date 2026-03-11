621043675fda7c944f4cba672828a98756e5a65b
kifludev <startc90@gmail.com>
2026-03-11 08:30:06 -0700

    Modifies pieces of the line of code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "kiflu-portfolio",
+  base: "/kiflu-portfolio/", // ✅ start and end with a slash
   server: {
-    port: 3000, // 👈 change to any port you want
+    port: 3000,
   },
 });
