4f550ce310e03c503230fa210d82d541ded0a2b7
kifludev <startc90@gmail.com>
2026-03-14 22:03:14 -0700

    update className

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -13,7 +13,7 @@ export function Navbar({ menuOpen, setMenuOpen }) {
           </a>
 
           <div
-            className="md:hidden m w-7 h-5 relative cursor-pointer z-40"
+            className="md:hidden w-7 h-5 relative cursor-pointer z-40"
             onClick={() => setMenuOpen((open) => !open)}
           >
             &#9776;
