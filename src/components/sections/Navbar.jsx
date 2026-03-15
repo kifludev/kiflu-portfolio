e3531cd209409d0203a27d2dbb07a945b7462b57
kifludev <startc90@gmail.com>
2026-03-14 19:41:20 -0700

    update the text to intended

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -9,12 +9,12 @@ export function Navbar({ menuOpen, setMenuOpen }) {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
-            Pedro <span className="text-blue-500">.tech</span>
+            Kiflu <span className="text-blue-500">.tech</span>
           </a>
 
           <div
             className="w-7 h-5 relative cursor-pointer z-40 md:hiddne"
-            onClick={() => setMenuOpen((prev) => !prev)}
+            onClick={() => setMenuOpen((open) => !open)}
           >
             &#9776;
           </div>
