dcdb3932d0c0e91e08dbec1a7e3e27ebac306260
kifludev <startc90@gmail.com>
2026-03-14 19:40:10 -0700

    update the text to the intended

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -7,8 +7,8 @@ export function Home() {
     >
       <RevealOnScroll>
         <div className="text-center px-4 z-10">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple 600 bg-clip-text text-transparent leading-right">
-            Hi, I'am pedro Tech
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-linear-to-r from-blue-500 to-purple 600 bg-clip-text text-transparent leading-right">
+            Hi, I'am kiflu Tech
           </h1>
 
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
